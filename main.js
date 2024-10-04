c2b3cfd62221f0a035bb10106857a8dd1e4437ba
Aisssaev <miroslavzizyn2006@gmail.com>
2024-10-04 21:45:17 +0300

    Added main.js

---
+++ main.js
@@ -0,0 +1,44 @@
+const { Command } = require('commander');
+const fs = require('fs');
+const path = require('path');
+
+const program = new Command();
+
+program
+    .requiredOption('-i, --input <path>', 'Path to the input file (JSON)')
+    .option('-o, --output <path>', 'Path to the output file')
+    .option('-d, --display', 'Display result to console')
+    .parse(process.argv);
+
+const options = program.opts();
+
+if (!fs.existsSync(options.input)) {
+    console.error('Cannot find input file');
+    process.exit(1);
+
+}
+
+try {
+    const data = 0;
+
+    if (!options.input) {
+        console.error('Please, specify input file');
+        process.exit(0);
+    }
+    if (!options.output && !options.display) {
+        process.exit(0);
+    }
+
+    if (options.output) {
+        fs.writeFileSync(options.output, result, 'utf8');
+        console.log(`Result has been written to ${options.output}`);
+    }
+
+    if (options.display) {
+        console.log(result);
+    }
+
+} catch (error) {
+    console.error('Error:', error.message);
+    process.exit(1);
+}
