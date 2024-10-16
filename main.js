badfdfdb7ee2ae7b624900f7453eeda456445c90
Aisssaev <miroslavzizyn2006@gmail.com>
2024-10-16 17:05:06 +0300

    fix

--- main.js
+++ main.js
@@ -3,24 +3,29 @@ const fs = require('fs');
 const path = require('path');
 
 const program = new Command();
+const code = 'utf8'
 
 program
-    .requiredOption('-i, --input <path>', 'Path to the input file (JSON)')
+    .option('-i, --input <path>', 'Path to the input file (JSON)')
     .option('-o, --output <path>', 'Path to the output file')
     .option('-d, --display', 'Display result to console')
     .parse(process.argv);
 
 const options = program.opts();
 
+if (!options.input) {
+    console.error('Please, specify input file');
+    process.exit(0);
+}
+
 if (!fs.existsSync(options.input)) {
     console.error('Cannot find input file');
     process.exit(1);
-
 }
 
 try {
     try {
-        const data = JSON.parse(fs.readFileSync(options.input, 'utf8'));
+        const data = JSON.parse(fs.readFileSync(options.input, code));
 
         const maxRate = Math.max(...data.map(item => item.rate));
 
@@ -30,16 +35,13 @@ try {
         console.error('Error:', error.message);
     }
 
-    if (!options.input) {
-        console.error('Please, specify input file');
-        process.exit(0);
-    }
+
     if (!options.output && !options.display) {
         process.exit(0);
     }
 
     if (options.output) {
-        fs.writeFileSync(options.output, result, 'utf8');
+        fs.writeFileSync(options.output, result, code);
         console.log(`Result has been written to ${options.output}`);
     }
 
